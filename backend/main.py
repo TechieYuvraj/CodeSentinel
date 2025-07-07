9393e95e0ce874d49dffb5ad8f95735657ae82a4
Yuvraj Singh <whitehat.hacker1108@gmail.com>
2025-07-07 21:47:15 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,13 @@
-
 from fastapi import FastAPI
-from scanner import scan_url, scan_repo
+from .scanner import get_security_headers
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the SecureCode Scanner API"}
+    return {"Hello": "World"}
 
-@app.post("/scan/url")
-def api_scan_url(url: str):
-    return scan_url(url)
-
-@app.post("/scan/repo")
-def api_scan_repo(repo_url: str):
-    return scan_repo(repo_url)
+@app.get("/scan-url/")
+def scan_url(url: str):
+    headers = get_security_headers(url)
+    return {"url": url, "security_headers": headers}
